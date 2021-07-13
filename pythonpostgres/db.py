d8e27c65706ba867b9fbc6fbf37bb4fb553ac6cc
slowstudent <38834547+slowstudent@users.noreply.github.com>
2021-07-13 10:46:25 +0600

    Add files via upload

---
+++ pythonpostgres/db.py
@@ -0,0 +1,22 @@
+DB_HOST = "localhost"
+DB_NAME = "postgres" 
+DB_USER = "postgres" 
+DB_PASS = "postgres"
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host=DB_HOST,
+    database=DB_NAME,
+    user=DB_USER,
+    password=DB_PASS)
+
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE createdwithpython (id SERIAL PRIMARY KEY, name VARCHAR)")
+cur.execute("INSERT INTO createdwithpython (name) VALUES(%s)", ("zxcvbnm",) )
+cur.execute("select * from createdwithpython where id=6;")
+print(cur.fetchall())
+
+conn.commit()
+conn.close()
